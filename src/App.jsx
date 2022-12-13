1afd91184ea79657fcb7ae1f41290f11f33ea627
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-12-13 19:54:57 +0200

    init 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,50 @@
 import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
-const animal_init = {
+
+const INIT_VALUE = {
     id: nanoid(),
-    type: null, // display this
-    journeysNo: 0, // display this
-    isHere: false, // display this
+    type: null,
+    journeysNo: 0,
+    isHere: false,
 };
 
-//<form> with one input field (Animal's type) and one button (save Animal type)
-//<button> that says either Go on a Journey or Return from the Journey (depends on
-//isHere)
-//
-//
 const App = () => {
     const [input, setInput] = useState('');
-    const [animal, setAnimal] = useState(animal_init);
+    const [animal, setAnimal] = useState(INIT_VALUE);
 
-    const toGoOnJourney = 'Go on a Journey';
-
-    const toGoBackJourney = 'Return from the Journey';
+    const { type, journeysNo, isHere } = animal;
 
     const handleInput = (event) => {
         setInput(event.target.value);
     };
 
-    const saveAnimalType = () => {
-        setAnimal((oldSpecs) => ({ ...oldSpecs, type: input }));
+    const addType = () => {
+        if (input === '') return;
+        setAnimal((oldSpecs) => ({ ...oldSpecs, type: input ? input : type }));
         setInput('');
     };
 
-    const toggleisHere = () => {
-        if (animal.isHere) {
-            setAnimal((oldSpecs) => ({
-                ...oldSpecs,
-                isHere: !animal.isHere,
-                journeysNo: animal.journeysNo + 1,
-            }));
-        } else {
-            setAnimal((oldSpecs) => ({ ...oldSpecs, isHere: !animal.isHere }));
-        }
+    const toggleStatus = () => {
+        setAnimal((oldSpecs) => ({
+            ...oldSpecs,
+            isHere: !isHere,
+            journeysNo: isHere ? journeysNo + 1 : journeysNo,
+        }));
     };
 
     return (
         <div>
             <input type="text" value={input} onChange={handleInput} />
-            <button onClick={saveAnimalType}>save Animal Type</button>
-            <button onClick={toggleisHere}>
-                {animal.isHere ? toGoOnJourney : toGoBackJourney}
+            <button onClick={addType}>Add Type</button>
+            <button onClick={toggleStatus}>
+                {isHere ? 'Come back from journey' : 'Go on a journey'}
             </button>
-            {animal.journeysNo}
-            {animal.type}
-            {!animal.isHere && 'Animal is Here'}
+            <h1>info About the animal</h1>
+            <ul>
+                <li>Animal type: {type}</li>
+                <li>Number of journeys: {journeysNo}</li>
+                <li>Status: {!isHere ? 'Animal is Here' : 'Animal is not here'}</li>
+            </ul>
         </div>
     );
 };
